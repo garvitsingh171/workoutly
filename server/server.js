cb61432d34b9a920bfb89a122c45918864f6d580
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2026-03-20 09:55:33 +0530

    Initized the monorepo setup of workoutly

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
